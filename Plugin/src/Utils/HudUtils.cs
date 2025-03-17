62ddc06d9bbf6a4d6aceee8e57b1b25d40cb621f
mattymatty <mattymatty@freemails.it>
2025-03-17 16:19:35 +0100

    skip `WaitForSeconds` if delay is 0

--- Plugin/src/Utils/HudUtils.cs
+++ Plugin/src/Utils/HudUtils.cs
@@ -8,14 +8,16 @@ public static class HudUtils
     internal static IEnumerator ShowMessageAfterDelay(string title, string text, float delay = 0f,
         bool isWarning = false)
     {
-        yield return new WaitForSeconds(delay);
+        if (delay > 0f)
+            yield return new WaitForSeconds(delay);
         yield return new WaitUntil(() => HUDManager.Instance.CanTipDisplay(isWarning, false, null));
         HUDManager.Instance.DisplayTip(title, text, isWarning);
     }
 
     internal static IEnumerator ShowTipAfterDelay(string title, string text, float delay, string saveKey)
     {
-        yield return new WaitForSeconds(delay);
+        if (delay > 0f)
+            yield return new WaitForSeconds(delay);
         yield return new WaitUntil(() => HUDManager.Instance.CanTipDisplay(false, false, null));
         HUDManager.Instance.DisplayTip(title, text, false, true, saveKey);
     }
